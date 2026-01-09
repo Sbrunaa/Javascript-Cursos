39839958dfbd183a9c246edccb4167664893b487
Bruna Santos <83190947+Sbrunaa@users.noreply.github.com>
2026-01-09 11:18:38 -0300

    Add files via upload

---
+++ javascript/Aula 02/aula02.js
@@ -0,0 +1,54 @@
+//tipos de dados(string, number, booleano e array), tipos de dados(string, number, booleano e array) e Operadores
+
+// tipos de dados(string, number, booleano e array)
+const name = "value";
+
+var Name = "value";
+
+// e tipos de dados(string, number, booleano e array)
+var empty = '';
+
+var number = 1;
+
+var True = true;
+var False = false;
+
+var objectList = {
+    day: '10',
+    month: 'january',
+    year: '2026'
+}
+
+//Operadores
+var valueA = 10;
+var valueB = 20;
+
+var sum = valueA + valueB; // resultado = 30
+
+var subtraction = valueA - valueB; // resultado = -10
+
+var multiplication = valueA * valueB; // resultado = 200
+
+var division = valueA / valueB; // resultado = 0.5
+
+//Operadores incremento e decremento
+
+var value1 = 100;
+
+value1++; // resultado = 101
+
+value1--; // resultado = 99
+
+var value2 = 100;
+
+value += 10; //resultado = 110
+
+value -= 10; // resultado = 90
+
+//Pegar o resto, que dar para rerificar se um número é impar ou par
+
+var valuePar = 8;
+var valueImpar = 7;
+
+valorPar % 2; // resultado = 0 ou seja é par
+valorImpar % 2; // resultado = 1 ou seja é impar
---
+++ javascript/Aula 05/aula05.js
@@ -0,0 +1,20 @@
+//eventos : onload, onsubmit, onchanges, onkeypress
+
+//onload -> é utilizado para disaar o evento quando toda a pagina e seus elementos estiverem carregados , ex:
+<html>
+    <body onload="main">
+        conteudo do site
+    </body>
+</html>
+
+//onsubmit -> é usado para validar o dados para enviar o formulario e noificar
+//  caso não tenho todos os dados impede o seu enviou. ex: <form onsubmit="myFunction()">
+
+
+//onchange -> usado para caixas de seleçoes e outros ele acontece sempre que um valor houver uma alteração.
+// ex: meuObjeto.onchange = function(){
+
+//onblur -> usando quando o usuario perde o foco, como num fomulario que so liberar o preenchimento da proxima
+//sessão apenas quando a anterior é preenchida.
+
+//onkeypress -> é usado para teclado é ativado quando uma tecla é precionada , menos a tecla ALT , CTRL , SHIFT e ESC
